75ade4f6941a3dd31ea25d9a574adde91b35901f
Marouane Boauricha <marouane.tchiko@gmail.com>
2024-08-16 18:08:20 +0100

    added error helper for permissions

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -142,3 +142,8 @@ func (app *application) background(fn func()) {
 		fn()
 	}()
 }
+
+func (app *application) notPermittedResponse(w http.ResponseWriter, r *http.Request) {
+	message := "your user account doesn't have the necessary permissions to access this resource"
+	app.errorResponse(w, r, http.StatusForbidden, message)
+}
